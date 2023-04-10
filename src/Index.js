5fa7c84090feae695868c6deac1359f4e1196d7a
MeganYippee <78123270+MeganYippee@users.noreply.github.com>
2023-04-10 16:31:30 -0700

    adding fonts and setting up new products page

--- src/Index.js
+++ src/Index.js
@@ -1,13 +1,14 @@
 let menu = document.querySelector(".menu");
-menu.innerHTML = `<img
+menu.innerHTML = `
+        <img
             class="menuLogo"
             src="src/photos/New+Logo.png"
             alt="Evolution Controls Logo"
          />
-         <a class="col" href="index.html">Home</a>
-         <a class="col" href="products.html">Products</a>
-         <a class="col" href="new-products.html">New Products</a>
-         <a class="col" href="datasheets.html">Datasheets</a>
-         <a class="col" href="distributors.html">Distributors</a>
-         <a class="col" href="tech-support.html">Tech Support</a>
-         <a class="col" href="applications.html">Applications</a>`;
+         <a class="col menu-title" href="index.html">Home</a>
+         <a class="col menu-title" href="products.html">Products</a>
+         <a class="col menu-title" href="new-products.html">New Products</a>
+         <a class="col menu-title" href="datasheets.html">Datasheets</a>
+         <a class="col menu-title" href="distributors.html">Distributors</a>
+         <a class="col menu-title" href="tech-support.html">Tech Support</a>
+         <a class="col menu-title" href="applications.html">Applications</a>`;
