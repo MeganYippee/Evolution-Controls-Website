39182eb40e657aedbddedd92e9e9578134ddea4d
MeganYippee <78123270+MeganYippee@users.noreply.github.com>
2023-04-10 13:33:07 -0700

    created menu bar in JS

--- src/Index.js
+++ src/Index.js
@@ -1,1 +1,13 @@
-function menu() {}
+let menu = document.querySelector(".menu");
+menu.innerHTML = `<img
+            class="menuLogo"
+            src="src/photos/New+Logo.png"
+            alt="Evolution Controls Logo"
+         />
+         <a class="col" href="index.html">Home</a>
+         <a class="col" href="products.html">Products</a>
+         <a class="col" href="new-products.html">New Products</a>
+         <a class="col" href="datasheets.html">Datasheets</a>
+         <a class="col" href="distributors.html">Distributors</a>
+         <a class="col" href="tech-support.html">Tech Support</a>
+         <a class="col" href="applications.html">Applications</a>`;
