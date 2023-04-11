d0523f86115b6ccd2faf6858c0fe65979f5858fb
MeganYippee <78123270+MeganYippee@users.noreply.github.com>
2023-04-11 16:05:25 -0700

    adjusting drop down menu

--- src/Index.js
+++ src/Index.js
@@ -1,17 +1,32 @@
-let menu = document.querySelector(".menu");
+let menu = document.querySelector(".nav-bar");
 menu.innerHTML = `
-        <img
-            class="menuLogo"
-            src="src/photos/New+Logo.png"
-            alt="Evolution Controls Logo"
-         />
+<div class="row menu">
          <a class="col menu-title" href="index.html">Home</a>
-         <a class="col menu-title" href="products.html">Products</a>
+          <div class="col menu-title dropdown">
+    <button class="dropbtn"> Products ▼
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Pro Series</a>
+      <a href="#">Wireless Series</a>
+      <a href="#">EC Motor Controls</a>
+      <a href="#">HVAC Automation</a>
+      <a href="#">Accessories and Cables</a>
+      <a href="#">Sales Support</a>
+    </div>
+  </div> 
          <a class="col menu-title" href="new-products.html">New Products</a>
          <a class="col menu-title" href="datasheets.html">Datasheets</a>
          <a class="col menu-title" href="distributors.html">Distributors</a>
          <a class="col menu-title" href="tech-support.html">Tech Support</a>
-         <a class="col menu-title" href="applications.html">Applications</a>`;
+         <span class="col menu-title dropdown">
+    <a class="dropbtn">Applications ▼
+    </a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+    </div>
+  </span>
+  </div> `;
 
 let footer = document.querySelector(`.footer`);
 footer.innerHTML = ` <p>
