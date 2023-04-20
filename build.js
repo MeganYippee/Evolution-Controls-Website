ea482f63880fa7663ad2b560b139bb421c19d214
Joseph Stewart <joseph.william.stewart@biola.edu>
2023-04-19 19:49:50 -0700

    Ignore template files.

--- build.js
+++ build.js
@@ -5,6 +5,10 @@ const fs = require('fs');
 const allPugFiles = glob.globSync('**/*.pug');
 
 for (const pugFile of allPugFiles) {
+	if (pugFile.startsWith('templates')) {
+		continue;
+	}
+
 	const result = pug.compileFile(pugFile)();
 	const htmlFile = pugFile.replace(/.pug$/, '.html');
 	fs.writeFileSync(htmlFile, result, { encoding: 'utf-8' });
