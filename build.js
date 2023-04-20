b25b697017a6ffe518ddfa1ff7d9fbc7eaba9536
Joseph Stewart <joseph.william.stewart@biola.edu>
2023-04-19 21:40:14 -0700

    Fix urls.

--- build.js
+++ build.js
@@ -1,13 +1,23 @@
 const glob = require('glob');
 const pug = require('pug');
 const fs = require('fs');
+const path = require('path');
 
 const pugDirectory = 'pug-src';
 
 const allPugFiles = glob.globSync(`${pugDirectory}/**/*.pug`);
 
 for (const pugFile of allPugFiles) {
-	const result = pug.compileFile(pugFile)();
-	const htmlFile = pugFile.replace(/.pug$/, '.html').substring(pugDirectory.length + 1);
+	const result = pug.compileFile(pugFile)({
+		baseUrl: process.env.BASE_URL ?? '/',
+	});
+
+	// Get the filepath for the html file
+	const htmlFile = path.join('build', pugFile.replace(/.pug$/, '.html').substring(pugDirectory.length + 1));
+
+	// Create all of the folders and subfolders that will contain the generated html file
+	fs.mkdirSync(path.dirname(htmlFile), { recursive: true });
+
+	// Create the html file itself
 	fs.writeFileSync(htmlFile, result, { encoding: 'utf-8' });
 }
