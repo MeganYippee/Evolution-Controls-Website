5b037ba1ed2b2d06357ef67e5e6462250ed0d5cb
Joseph Stewart <joseph.william.stewart@biola.edu>
2023-04-19 22:13:01 -0700

    Copy src directory to build output.

--- build.js
+++ build.js
@@ -3,13 +3,20 @@ const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
 
+// Where the pug site lives
 const pugDirectory = 'pug-src';
+
+// Where assets like stylesheets and javascript files live
+const publicDirectory = 'src';
+
+// Copy public assets to build output
+fs.cpSync(publicDirectory, `build/${publicDirectory}`, { recursive: true });
 
 const allPugFiles = glob.globSync(`${pugDirectory}/**/*.pug`);
 
 for (const pugFile of allPugFiles) {
 	const result = pug.compileFile(pugFile)({
-		baseUrl: process.env.BASE_URL ?? '/',
+		baseUrl: process.env.BASE_URL ?? '/build/',
 	});
 
 	// Get the filepath for the html file
