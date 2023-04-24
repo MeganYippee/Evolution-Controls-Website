26d2077f4df8016203b251ae5b9e5f10c4c119f3
josephdangerstewart <josephlikescode@gmail.com>
2023-04-24 16:48:38 -0700

    Fix deployed site (#2)
    * Fix urls.
    
    * Delete committed generated html files.
    
    * Add workflow to deploy pages app.
    
    * Fix build command.
    
    * Copy src directory to build output.

--- build.js
+++ build.js
@@ -1,13 +1,30 @@
 const glob = require('glob');
 const pug = require('pug');
 const fs = require('fs');
+const path = require('path');
 
+// Where the pug site lives
 const pugDirectory = 'pug-src';
+
+// Where assets like stylesheets and javascript files live
+const publicDirectory = 'src';
+
+// Copy public assets to build output
+fs.cpSync(publicDirectory, `build/${publicDirectory}`, { recursive: true });
 
 const allPugFiles = glob.globSync(`${pugDirectory}/**/*.pug`);
 
 for (const pugFile of allPugFiles) {
-	const result = pug.compileFile(pugFile)();
-	const htmlFile = pugFile.replace(/.pug$/, '.html').substring(pugDirectory.length + 1);
+	const result = pug.compileFile(pugFile)({
+		baseUrl: process.env.BASE_URL ?? '/build/',
+	});
+
+	// Get the filepath for the html file
+	const htmlFile = path.join('build', pugFile.replace(/.pug$/, '.html').substring(pugDirectory.length + 1));
+
+	// Create all of the folders and subfolders that will contain the generated html file
+	fs.mkdirSync(path.dirname(htmlFile), { recursive: true });
+
+	// Create the html file itself
 	fs.writeFileSync(htmlFile, result, { encoding: 'utf-8' });
 }
