538c6e0aba5062942968b8a7d046ce47de6b265f
MeganYippee <78123270+MeganYippee@users.noreply.github.com>
2023-10-10 16:30:03 -0700

    setting up js

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,1 +1,19 @@
 const mobileMenu = window.matchMedia("(max-width: 428px)");
+
+if (mobileMenu.matches) {
+   let menu = document.querySelector("");
+   console.log("Media Query Matched!");
+}
+
+// Get HTML head element
+let head = document.getElementsByTagName("HEAD")[0];
+
+// Create new link Element
+let link = document.createElement("link");
+
+// set the attributes for link element
+link.rel = "stylesheet";
+
+link.type = "text/css";
+
+link.href = "style.css";
