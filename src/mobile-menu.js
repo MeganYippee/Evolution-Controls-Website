0cc7cf519970af7406cfee973b5ac93d817cd6ac
MeganYippee <78123270+MeganYippee@users.noreply.github.com>
2023-12-20 23:00:22 +0000

    Deploying to gh-pages from @ MeganYippee/Evolution-Controls-Website@b0b0d0fbf6bf9232fddaa516c9691a63d8066927 🚀

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function displayLinks() {
    var x = document.getElementById("nav-links");
    if (x.style.display === "block") {
       x.style.display = "none";
@@ -6,3 +6,21 @@ function myFunction() {
       x.style.display = "block";
    }
 }
+
+function myFunction(x) {
+   if (x.matches) {
+      // If media query matches
+      console.log(navLinks.getAttribute("id"));
+   } else {
+      document.body.style.backgroundColor = "pink";
+   }
+}
+
+const navLinks = document.getElementById("nav-links");
+var x = window.matchMedia("(min-width: 428px)");
+
+myFunction(x);
+
+x.addEventListener("change", function () {
+   myFunction(x);
+});
