a1fa079725ffe20c556da4bae0ec47e2da5c59d0
MeganYippee <78123270+MeganYippee@users.noreply.github.com>
2024-05-20 16:46:04 -0700

    mobile menu fix

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -6,20 +6,3 @@ function displayLinks() {
       x.style.display = "flex";
    }
 }
-
-function myFunction(x) {
-   if (x.matches) {
-      // If media query matches
-      navLinksDesktop.style.display = "none";
-   } else {
-   }
-}
-
-const navLinksDesktop = document.getElementById("#nav-links-desktop");
-var x = window.matchMedia("(min-width: 600px)");
-
-myFunction(x);
-
-x.addEventListener("change", function () {
-   myFunction(x);
-});
